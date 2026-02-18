13e9c740c2dc4fb6eab493264593fdadb4963d91
Poguri Diya <diya>
2026-02-18 14:18:43 +0530

    terraform

--- main.tf
+++ main.tf
@@ -198,7 +198,7 @@ resource "aws_lb_listener_rule" "main" {
   }
 }
 
-resource "terraform_data" "main" {
+resource "terraform_data" "main_local" {
   triggers_replace = [
     aws_instance.main.id
   ]
