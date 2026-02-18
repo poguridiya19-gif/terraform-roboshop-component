f7cb4f659144d837d34c6091e9adc0be7d34bcb3
Poguri Diya <diya>
2026-02-18 14:36:30 +0530

    terraform

--- data.tf
+++ data.tf
@@ -19,7 +19,7 @@ data "aws_ami" "joindevops" {
 }
 
 data "aws_ssm_parameter" "sg_id" {
-  name = "/${var.project_name}/${var.environment}/sg_id"
+  name = "/${var.project_name}/${var.environment}/${var.component}/sg_id"
 }
 
 data "aws_ssm_parameter" "private_subnet_ids" {
